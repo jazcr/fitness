4fcf77e0d7a7a4699c7c2e465046ec2d5aba8574
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-08-31 17:45:15 -0600

    server/db connecting/running

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const mongoose = require("mongoose");
 const express = require("express");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/db", { useNewUrlParser: true });
+const PORT = process.env.port || 3004;
+
+//added options to silence depracation warnings
+mongoose.connect("mongodb://localhost:27017/fitnessDb", { useNewUrlParser: true, useUnifiedTopology: true,
+useCreateIndex: true})
+    .then(() => console.log("Connected to db!"))
+    .catch(e => console.log(e));
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/`));
